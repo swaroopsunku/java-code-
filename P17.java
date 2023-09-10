89c6d30060a3abc6da536f3f2cc5a3f8d188db87
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-10 11:30:46 +0530

    ELEMENTS IN ASCENDING ORDER

---
+++ P17.java
@@ -0,0 +1,28 @@
+package arrays;
+import java.util.*;
+public class P17 {
+	static Scanner scan=new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.print("Enter the size of an array :");
+		int size=scan.nextInt();
+		int[] a=new int[size];
+		for (int i = 0; i < a.length; i++) {
+			System.out.print("Enter the "+(i+1)+" element :");
+			a[i]=scan.nextInt();
+		}
+		for (int i = 0; i < a.length-1; i++) {
+			for (int j = 1; j < a.length-i; j++) {
+				if (a[j-1]>a[j]) {
+					int temp=a[j-1];
+					a[j-1]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+		System.out.print('[');
+		for (int i : a) {
+			System.out.print(i+",");
+		}
+		System.out.println(']');
+	}
+}
