6798e8bdfd80f3c76e478a1de3d3a214df7d3f1c
chandra swaroop <chandraswaroop6630@gmail.com>
2023-09-10 11:22:30 +0530

    AVAREGE VALUE OF ODD INDEX

---
+++ P11.java
@@ -0,0 +1,23 @@
+package arrays;
+import java.util.*;
+public class P11 {
+	static Scanner scan=new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.print("Enter the size of an array :");
+		int size=scan.nextInt();
+		int[] a=new int[size];
+		for (int i = 0; i < a.length; i++) {
+			System.out.print("Enter the "+(i+1)+" element :");
+			a[i]=scan.nextInt();
+		}
+		int sum=0,count=0;
+		for(int j=0;j<a.length;j++) {
+			if (j%2==1) {
+				sum+=a[j];
+				count++;
+			}
+		}
+		int avg=sum/count;
+		System.out.println(avg);
+	}
+}
